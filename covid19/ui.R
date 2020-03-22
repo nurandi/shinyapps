cee6408b612bbcf8e58da40586746b24bc6e6814
Nur Andi Setiabudi <nurandi.mail@gmail.com>
2020-03-22 13:19:16 -0400

    update about
    replace hardcoce HTML with markdown

--- covid19/ui.R
+++ covid19/ui.R
@@ -13,7 +13,7 @@ dashboardPage(skin="blue",
     sidebarMenu(
       menuItem("Dashboard", tabName = "dashboard"),
       menuItem("Raw data", tabName = "rawdata"),
-      menuItem("About", tabName = "about")  )
+      menuItem("About", tabName = "about"))
     
   ),
   
@@ -54,7 +54,8 @@ dashboardPage(skin="blue",
                   width = 12, status = "info", solidHeader = TRUE,
                   title = "Daily Cases",
                   plotlyOutput("historyChart", width = "100%", height = 400))
-              )
+              ),
+              
       ), 
       
       #menu2 : raw data & download
@@ -67,33 +68,7 @@ dashboardPage(skin="blue",
       ),
       
       #menu2 : about
-      tabItem("about",
-              
-              tags$h3("About this Dashboard"),
-              
-              HTML(
-                
-                paste('',
-                      'Simple dashboard to present COVID-19 pandemic',
-                      '',
-                      'Built in R using shiny dashboard, leaflet, plotly, data table',
-                      '',
-                      'Data source:',
-                      '<a href="https://github.com/CSSEGISandData/COVID-19">Johns Hopkins CSSE</a>',
-                      '',
-                      'Creator:',
-                      'Nur Andi Setiabudi',
-                      '<a href="https://nurandi.id">NURANDI.ID</a>',
-                      '<a href="mailto:nurandi.mail@gmail.com">nurandi.mail@gmail.com</a>',
-                      '',
-                      'License:',
-                      '<a href="https://opensource.org/licenses/MIT">MIT License</a>',
-                      '',
-                      '(c) 2020',
-                      sep='<br/>')
-              )
-              
-      )
+      tabItem("about", includeMarkdown("README.md") )
       
     )
     
